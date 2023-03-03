f570c2ce3f03591e84b0480264f3ce875b04df68
Mario Martinez <mario3alberto3@gmail.com>
2023-03-02 22:48:16 -0600

    added two authentication classes in REST_FRAMEWORK

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -125,5 +125,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
 
     # Add code to assign default authentication classes
-    
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+    'rest_framework.authentication.TokenAuthentication',
+    'rest_framework.authentication.SessionAuthentication',
+    ],
 }
