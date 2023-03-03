d794bf681e77610c7ad0869ed8bbb67de4c17667
Mario Martinez <mario3alberto3@gmail.com>
2023-03-03 00:30:01 -0600

    substituted a new RatingsView class

--- LittleLemonDRF/views.py
+++ LittleLemonDRF/views.py
@@ -3,10 +3,15 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Rating
 from .serializers import RatingSerializer
+from rest_framework.views import ModelViewSet
 
-class RatingsView(generics.ListCreateAPIView):
+class RatingsView(ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+
+#class RatingsView(generics.ListCreateAPIView):
+#    queryset = Rating.objects.all()
+#    serializer_class = RatingSerializer
 
 def get_permissions(self):
     if(self.request.method=='GET'):
